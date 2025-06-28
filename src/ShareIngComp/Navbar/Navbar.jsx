d6479b2e0ddd2e35720f3049e00f7a2362d6a41b
vernoxy <vernoxy3@gmail.com>
2025-06-28 16:43:18 +0530

    first commit

--- src/ShareIngComp/Navbar/Navbar.jsx
+++ src/ShareIngComp/Navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className="w-full  bg-white shadow-sm  top-0 z-50">
-      <div className=" mx-auto flex justify-between items-center h-32 px-4">
+      <div className=" mx-auto flex justify-between items-center sm:h-32 px-4">
         {/* Right Div */}
         <div className=" hidden md:block w-full max-w-[160px] h-12 bg-gradient-to-l from-[#F29014] to-white ml-6 mr-6 " />
 
