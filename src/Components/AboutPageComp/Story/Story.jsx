3d43a59213f62d21ffe0d50b53c6f2feda20efb2
vernoxy <vernoxy3@gmail.com>
2025-07-01 17:37:57 +0530

    Daytoday Work

--- src/Components/AboutPageComp/Story/Story.jsx
+++ src/Components/AboutPageComp/Story/Story.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Docters from "../../../assets/AboutUsAssets/docters.webp";
+import Docters from "../../../assets/AboutUsAssets/Docters.webp";
 
 const Story = () => {
   return (
