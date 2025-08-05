194b2b8be16fdd5b88aba3be08323a10c3d1ecb0
vernoxy <vernoxy3@gmail.com>
2025-08-05 17:42:05 +0530

    Change on 0508

--- src/Components/ConatactUsPageComp/Help/Help.jsx
+++ src/Components/ConatactUsPageComp/Help/Help.jsx
@@ -9,46 +9,46 @@ const ContactData = [
     id: 1,
     Icon: Location,
     title: "Location",
-    dec: "Shop No 1, BDCA Building, BHIDBHANJAN MAHADEV MANDIR, KAPADIA CHAL, Abrama-396002",
+    description: "Shop No 1, BDCA Building, BHIDBHANJAN MAHADEV MANDIR, KAPADIA CHAL, Abrama-396002",
     link: "https://www.google.com/maps?q=PUNARVASU+AYURVEDA+CHIKITSALAYAM,+Shop+No+1,+BDCA+Building,+BHIDBHANJAN+MAHADEV+MANDIR,+KAPADIA+CHAL,+Abrama-396002",
   },
   {
     id: 2,
     Icon: Call,
     title: "Call Us",
-    dec: "Phone No.: +91 07947 112761",
+    description: "Phone No.: +91 07947 112761",
     link: "tel:07947112761",
   },
   {
     id: 3,
     Icon: Email,
     title: "Email",
-    dec: "Mail Now : punarvasu@gmail.com",
+    description: "Mail Now: punarvasu@gmail.com",
     link: "mailto:punarvasu@gmail.com",
   },
 ];
 
 const Help = () => {
   return (
-    <section className="py-20 space-y-12">
+    <div className="py-20 space-y-12">
       {/* Heading */}
-      <div className="text-center space-y-6 px-4">
+      <div className="text-start container space-y-2 pb-5">
         <h1
           data-aos="fade-up"
-          className="text-3xl sm:text-4xl lg:text-5xl font-bold text-secondary"
+          className="text-3xl sm:text-4xl lg:text-5xl font-bold text-textColor"
         >
-          Need Any Help?
+          Dr. Yogesh H. Desai
         </h1>
         <p
           data-aos="fade-up"
           className="lg:text-2xl text-textColor font-medium"
         >
-          We’re Just a Message Away – Let’s Talk!
+         B.A.M.S., M.D. (Ayu)
         </p>
       </div>
 
       {/* Contact Cards */}
-      <div className="container mx-auto px-4 grid gap-10 sm:grid-cols-2 xl:grid-cols-3">
+      <div className="container mx-auto grid gap-10 sm:grid-cols-2 xl:grid-cols-3">
         {ContactData.map((item) => (
           <a
             key={item.id}
@@ -59,7 +59,7 @@ const Help = () => {
           >
             <div
               data-aos="fade-left"
-              className="text-center bg-cover bg-no-repeat bg-center rounded-3xl p-6 sm:p-6 lg:p-10 flex flex-col justify-center items-center w-full min-h-[300px] sm:min-h-[305px] lg:min-h-[400px] transition-transform duration-300 hover:scale-[1.02]"
+              className="text-center bg-cover bg-no-repeat bg-center rounded-3xl p-6 sm:p-6 lg:p-10 flex flex-col justify-center items-center w-full min-h-[300px] sm:min-h-[305px] lg:min-h-[330px] 2xl:min-h-[370px] transition-transform duration-300 hover:scale-[1.02]"
               style={{ backgroundImage: `url(${HelpBg})` }}
             >
               <img
@@ -68,11 +68,11 @@ const Help = () => {
                 className="mx-auto h-16 w-16 sm:h-20 sm:w-20 group-hover:scale-110 duration-300 transition-transform"
               />
               <div className="mt-8 space-y-3">
-                <h3 className="text-xl sm:text-2xl lg:text-3xl font-bold text-primary transition">
+                <h3 className="text-xl sm:text-2xl lg:text-3xl font-bold text-primary">
                   {item.title}
                 </h3>
-                <p className="text-textColor text-base transition max-w-xs mx-auto">
-                  {item.dec}
+                <p className="text-textColor text-base max-w-xs mx-auto">
+                  {item.description}
                 </p>
               </div>
             </div>
@@ -81,8 +81,8 @@ const Help = () => {
       </div>
 
       {/* Map Section */}
-      <section className="py-5 ">
-        <div className="container relative z-10">
+      <div className="py-5">
+        <div className="container px-0 relative z-10">
           <iframe
             data-aos="fade"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1026.1510910643985!2d72.92953194045963!3d20.612237118456523!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be0e8278e326ecd%3A0xe242834ab094f701!2sPUNARVASU%20AYURVEDA%20CHIKITSALAYAM%20-%20Best%20Panchakarma%20%7C%20Suvarnaprashan%20%7C%20Cosmetic%20%7C%20Garbh%20Sanskar%20Center%20in%20Valsad!5e0!3m2!1sen!2sin!4v1751258891306!5m2!1sen!2sin"
@@ -94,8 +94,8 @@ const Help = () => {
             title="Punarvasu Ayurveda Chikitsalayam Map"
           ></iframe>
         </div>
-      </section>
-    </section>
+      </div>
+    </div>
   );
 };
 
